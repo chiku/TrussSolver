e60907c15dd1c374e9e923a9ff934147e7890b67
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-05-15 12:21:24 +0530

    Consistent naming for data files

--- tests/truss_one_test.cpp
+++ tests/truss_one_test.cpp
@@ -7,7 +7,7 @@
 void TrussTest::test_truss_one()
 {
     Truss truss;
-    truss.getData("data/truss.dat");
+    truss.getData("data/truss1.dat");
     truss.findKLocal();
     truss.findKGlobal();
     truss.condense();
